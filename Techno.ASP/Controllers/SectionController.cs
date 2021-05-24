fd0bbacc5cf72c567f8c6472c589dedc9747aea1
isaskou <isabel.skou@yahoo.fr>
2021-05-24 14:04:07 +0200

    add Services, Controllers and Views for Section

---
+++ Techno.ASP/Controllers/SectionController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Techno.ASP.Models;
+using Techno.ASP.Models.FormsModel;
+using Techno.ASP.Services;
+
+namespace Techno.ASP.Controllers
+{
+    public class SectionController : Controller
+    {
+        private readonly SectionService _service;
+
+        public SectionController(SectionService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult AllSections()
+        {
+            IEnumerable<SectionModel> models = _service.GetAll();
+            return View(models);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(SectionForm form)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.Insert(form);
+                TempData["success"] = "Une nouvelle filière a été créée";
+                return RedirectToAction(nameof(AllSections));
+            }
+            else
+            {
+                TempData["error"] = "La filière n'a pas été créée, Veuillez recommencer";
+                return View(form);
+            }
+        }
+
+        public IActionResult Update(int id)
+        {
+            SectionForm form = _service.GetById(id);
+
+            if (form == null)
+            {
+                return NotFound();
+            }
+            return View(form);
+        }
+
+        [HttpPost]
+        public IActionResult Update(SectionForm form)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.Update(form);
+                TempData["success"] = "La filière a été mise à jour";
+                return RedirectToAction(nameof(AllSections));
+            }
+            else
+            {
+                return View(form);
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (_service.Delete(id))
+            {
+                TempData["success"] = "La filière à été supprimée";
+            }
+            else
+            {
+                TempData["error"] = "Une erreur est survenue, veuillez recommencer";
+            }
+            return RedirectToAction(nameof(AllSections));
+        }
+    }
+}
---
+++ Techno.ASP/Services/SectionService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Techno.ASP.Models;
+using Techno.ASP.Models.FormsModel;
+using Techno.DAL;
+using Techno.DAL.Entities;
+
+namespace Techno.ASP.Services
+{
+    public class SectionService
+    {
+        private readonly DataContext _dc;
+
+        public SectionService(DataContext dc)
+        {
+            _dc = dc;
+        }
+
+        public IEnumerable<SectionModel> GetAll()
+        {
+            IEnumerable<Section> allSections = _dc.Sections;
+
+            return allSections.Select(s => new SectionModel
+            {
+                Id = s.Id,
+                Name = s.Name,
+                Description = s.Description
+            });
+        }
+
+        public void Insert(SectionForm form)
+        {
+            Section s = new Section
+            {
+                Id = form.Id,
+                Name = form.Name,
+                Description = form.Description
+            };
+
+            _dc.Sections.Add(s);
+            _dc.SaveChanges();
+        }
+
+        public SectionForm GetById(int id)
+        {
+            Section s = _dc.Sections.Find(id);
+
+            if (s != null)
+            {
+                return new SectionForm
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    Description = s.Description
+                };
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void Update(SectionForm form)
+        {
+            Section s = _dc.Sections.Find(form.Id);
+            s.Name = form.Name;
+            s.Description = form.Description;
+            _dc.SaveChanges();
+        }
+
+        internal bool Delete(int id)
+        {
+            Section toDelete = _dc.Sections.Find(id);
+
+            if (toDelete == null)
+            {
+                return false;
+            }
+            else
+            {
+                _dc.Remove(toDelete);
+                _dc.SaveChanges();
+                return true;
+            }
+        }
+    }
+}
