bd0026271ccc1cfe2f794eba3e5e2a9a81ffb55b
isaskou <isabel.skou@yahoo.fr>
2021-05-29 10:41:00 +0200

    add an interface IServices

---
+++ Techno.ASP/Services/IServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Techno.ASP.Services
+{
+    public interface IServices<T,U>
+        where T:class
+        where U:class
+    {
+        bool Delete(int id);
+        IEnumerable<T> GetAll();
+        U GetById(int id);
+        void Insert(U form);
+        void Update(U form);
+    }
+}
